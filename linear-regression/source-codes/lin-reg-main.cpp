0b436137da6fdc0c6c1d39166a38b00c446316f7
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-09-26 05:04:03 +0800

    Changed the type declaration and added class method definitions

--- linear-regression/source-codes/lin-reg-main.cpp
+++ linear-regression/source-codes/lin-reg-main.cpp
@@ -3,45 +3,62 @@
 #include <string>
 #include <armadillo>
 
-enum Errors = {
-    OutOfBoundsException
-};
+using namespace arma;
 
 class LinearRegression {
     public:
-        Row<float>;
+        rowvec weights;
         float bias;
         int epochs;
         float learning_rate;
         bool fit_intercept;
 
-        void fit (Mat<float> train_x, Row<float> train_y);
-        Row<float> predict (Mat<float> test_x);
+        void fit (mat train_x, rowvec train_y);
+        rowvec predict (mat test_x);
 
     private:
-        void compute_weights_gradients (Mat<float> train_x, Row<float> train_y, Row<float> predictions);
-        void compute_bias_gradients (Row<float> train_y, Row<float> predictions);
-        void update_weights (Row<float> computed_weight_gradients);
+        void compute_weights_gradients (mat train_x, rowvec train_y, rowvec predictions);
+        void compute_bias_gradients (rowvec train_y, rowvec predictions);
+        void update_weights (rowvec computed_weight_gradients);
         void update_bias (float copmputed_bias_gradient);
 };
 
 LinearRegression::LinearRegression (int epochs, float learning_rate, bool fit_intercept) {
-    if (epochs <= 0) {
-        printf(Errors::OutOfBoundsException);
+    if (epochs == 0) {
         epochs = 25;
     }
     if (learning_rate <= 0.0001) {
-        printf(Errors::OutOfBoundsException);
         learning_rate = 0.0001;
     }
+    
     fit_intercept = fit_intercept;
+    Row<float> weights;
+    float bias; 
 };
 
-void fit (Mat<float> train_x, Row<float> train_y) {
+void LinearRegression::compute_weights_gradients (mat train_x, rowvec train_y, rowvec predictions) {
+    double weights_gradient = 1 / train_x.n_rows() * dot(train_x.t(), (predictions - train_y));
+    return weights_gradient;
+}
+
+void LinearRegression::compute_bias_gradients (rowvec train_y, rowvec predictions) {
+    double bias_gradient = 1 / train_y.n_rows() * sum((predictions - train_y));
+    return bias_gradient;
+}
+
+void LinearRegression::update_weights (double weights_gradient) {
 
 }
 
-void predict (Row<float> train_y, Row<float> predictions) {
+void LinearRegression::update_bias (double bias_gradient) {
+
+}
+
+void LinearRegression::fit (mat train_x, rowvec train_y) {
+
+}
+
+void LinearRegression::predict (mat test_x) {
 
 }
 
