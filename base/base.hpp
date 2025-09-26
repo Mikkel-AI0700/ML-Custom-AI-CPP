9a8a0069e75f5a961e73684361d2335e2290ff24
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-09-26 11:22:41 +0800

    Added the expected parameters for each function declaration

--- base/base.hpp
+++ base/base.hpp
@@ -4,19 +4,30 @@
 #include <any>
 #include <map>
 #include <string>
+#include <armadillo>
+
+using namespace arma;
 
 class BaseEstimator {
     public:
         virtual std::map<std::string, any> get_parameters ();
-        virtual void set_parameters ();
+        virtual void set_parameters (
+            std::map<std::string, any> original_hyperparameters,
+            std::map<std::string, any> user_hyperparameters
+        );
 
-        virtual void fit ();
-        virtual void predict ();
-        virtual void predict_proba ();
-        virtual void predict_proba_log ();
+        virtual void fit (mat train_x, rowvec train_y);
+        virtual void predict (mat test_x);
+        virtual void predict_proba (mat test_x);
+        virtual void predict_proba_log (mat test_x);
 
-        virtual void save_model ();
-        virtual void load_model ();
+        virtual void save_model (
+            std::string model_save_name,
+            std::filesystem::path save_path
+        );
+        virtual void load_model (
+            std::filesystem::path load_path
+        );
 };
 
 #endif
