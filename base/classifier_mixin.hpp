4dd903c54b3fe753124665ce8dd1f2d5af4ae5bd
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-09-26 15:36:39 +0800

    Created the base for the ClassifierMixin class

---
+++ base/classifier_mixin.hpp
@@ -0,0 +1,15 @@
+#ifndef CLASSIFIER_MIXIN_HPP
+#define CLASSIFIER_MIXIN_HPP
+
+#include <any>
+#include <map>
+#include <string>
+#include <armadillo>
+
+using namespace std;
+
+class ClassifierMixin {
+};
+
+#endif
+
