9f1bd7f73e031cd29760e3f905b4a6de217c8d65
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-09-28 18:47:37 +0800

    Coded the main logic for the functions to create the datasets

--- python-data-generators/generator-files/generator.py
+++ python-data-generators/generator-files/generator.py
@@ -3,24 +3,33 @@
 import os
 import json
 import argparse
+import pandas as pd
 from sklearn.datasets import (
     make_regression,
     make_classification,
-    make_multilabel_classification,
     make_blobs
 )
 
-def _read_generator_configuration (filepath: str):
-    pass
+def _read_generator_configuration (json_configuration_path: str):
+    return json.load(json_configuration_path)
 
-def create_regression ():
-    pass
+def _write_to_file (filename: str, generated_dataset: pd.DataFrame):
+    generated_dataset.to_csv(filename)
 
-def create_classification ():
-    pass
+def create_regression (dataset_filename: str, regressor_config: str):
+    regression_config_dict = _return_generator_configuration(regressor_config)
+    regressor_dataset = make_regression(**regression_config_dict)
+    _write_to_file(dataset_filename, pd.DataFrame(regressor_dataset))
 
-def create_clustering ():
-    pass
+def create_classification (dataset_filename: str, classification_config: str):
+    classification_config_dict = _return_generator_configuration(classification_config)
+    classification_dataset = make_classification(**classification_config_dict)
+    _write_to_file(dataset_filename, pd.DataFrame(classification_dataset))
+
+def create_clustering (dataset_filename: str, clustering_config: str):
+    clustering_config_dict = _return_generator_configuration(clustering_config)
+    clustering_dataset = make_blobs(**clustering_config_dict)
+    _write_to_file(dataset_filename, pd.DataFrame(clustering_dataset))
 
 def main ():
     argp = argparse.ArgumentParser(description="ML dataset generators")
@@ -28,4 +37,20 @@ def main ():
     argp.add_argument("--dataset_type", required=True, dest="dset_type")
     argp.add_argument("--use-default", required=False, action="store_true")
     argp.add_argument("--key-value-change", required=False, dest="key_value_parameter")
+    argp.add_argument("--dataset-filename", required=False, dset="dataset_filename")
+
+    parsed_arguments = argp.parse_args()
+
+    try:
+        if parsed_arguments.dset_type == "regression":
+            pass
+        elif parsed_arguments.dset_type == "classification":
+            pass
+        elif parsed_arguments.dset_type == "clustering":
+            pass
+        else:
+            raise ValueError(f"[-] Error: Incorrect dataset type -> {parsed_argument.dset_type}")
+    except ValueError as incorrect_argument_error:
+        print(incorrect_argument_error)
+        exit(1)
 
