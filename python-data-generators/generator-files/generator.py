1898c0809ace4f47587c26102b69108c02d97e25
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-10-01 18:17:10 +0800

    Added the key_value_config parameter, condition to check if key_value_config has length greater than 0 and standardized the call of _change_configuration on all functions

--- python-data-generators/generator-files/generator.py
+++ python-data-generators/generator-files/generator.py
@@ -21,6 +21,9 @@ def _write_to_file (filename: str, generated_dataset: pd.DataFrame):
 def _change_configuration (config_key_value: dict[str, Any], generator_configuration: dict[str, Any]):
     config_key, config_value = config_key_value.items()
     try:
+        if len(key_config_value) == 0:
+            return
+
         if config_key in generator_configuration.keys():
             generator_configuration.update({config_key: config_value})
         else:
@@ -29,18 +32,21 @@ def _change_configuration (config_key_value: dict[str, Any], generator_configura
         print(non_existent_config_key)
         exit(1)
 
-def create_regression (dataset_filename: str, regressor_config: Path):
+def create_regression (dataset_filename: str, regressor_config: Path, key_value_config: dict[str, Any]):
     regression_config_dict = _return_generator_configuration(regressor_config)
+    _change_configuration(key_value_config, regression_config_dict)
     regressor_dataset = make_regression(**regression_config_dict)
     _write_to_file(dataset_filename, pd.DataFrame(regressor_dataset))
 
-def create_classification (dataset_filename: str, classification_config: Path):
+def create_classification (dataset_filename: str, classification_config: Path, key_value_config: dict[str, Any]):
     classification_config_dict = _return_generator_configuration(classification_config)
+    _change_configuration(key_value_config, classification_config_dict)
     classification_dataset = make_classification(**classification_config_dict)
     _write_to_file(dataset_filename, pd.DataFrame(classification_dataset))
 
-def create_clustering (dataset_filename: str, clustering_config: Path):
+def create_clustering (dataset_filename: str, clustering_config: Path, key_value_config: dict[str, Any]):
     clustering_config_dict = _return_generator_configuration(clustering_config)
+    _change_configuration(key_value_config, clustering_config_dict)
     clustering_dataset = make_blobs(**clustering_config_dict)
     _write_to_file(dataset_filename, pd.DataFrame(clustering_dataset))
 
