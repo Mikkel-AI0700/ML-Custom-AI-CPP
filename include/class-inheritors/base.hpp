87240ca3d8b48ac897fa229387c8051720f95fc9
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-10-18 21:17:54 +0800

    Just some basic fixing of formats

--- include/class-inheritors/base.hpp
+++ include/class-inheritors/base.hpp
@@ -15,23 +15,15 @@ class BaseEstimator {
         ~BaseEstimator() = default;
 
         virtual HashMapParameters get_parameters (HashMapParameters hyperparameter_hashmap);
-        virtual void set_parameters (
-            HashMapParameters original_hyperparameters,
-            HashMapParameters user_hyperparameters
-        );
+        virtual void set_parameters (HashMapParameters original_hyperparameters, HashMapParameters user_hyperparameters);
 
         virtual void fit (arma::mat train_x, arma::rowvec train_y);
         virtual arma::rowvec predict (arma::mat test_x);
         virtual arma::rowvec predict_proba (arma::mat test_x);
         virtual arma::rowvec predict_proba_log (arma::mat test_x);
 
-        virtual void save_model (
-            std::string model_save_name,
-            std::filesystem::path save_path
-        );
-        virtual void load_model (
-            std::filesystem::path load_path
-        );
+        virtual void save_model (std::string model_save_name, std::filesystem::path save_path);
+        virtual void load_model (std::filesystem::path load_path);
 };
 
 #endif
