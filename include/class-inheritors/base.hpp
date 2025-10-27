4f9509bf198c99e62c441b9fa267f5ba72215127
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-10-27 14:24:31 +0800

    Added the ampersand symbol to pass-by-reference than pass-by-value

--- include/class-inheritors/base.hpp
+++ include/class-inheritors/base.hpp
@@ -1,10 +1,6 @@
 #ifndef BASE_ESTIMATOR_HPP
 #define BASE_ESTIMATOR_HPP
 
-#include <any>
-#include <variant>
-#include <tuple>
-#include <map>
 #include <string>
 #include <filesystem>
 #include <armadillo>
@@ -19,8 +15,8 @@ class BaseEstimator {
 
         virtual void fit (arma::mat& train_x, arma::colvec& train_y);
         virtual arma::rowvec predict (arma::mat& test_x);
-        virtual arma::rowvec predict_proba (arma::mat test_x);
-        virtual arma::rowvec predict_proba_log (arma::mat test_x);
+        virtual arma::rowvec predict_proba (arma::mat& test_x);
+        virtual arma::rowvec predict_proba_log (arma::mat& test_x);
 
         virtual void save_model (std::string model_save_name, std::filesystem::path save_path);
         virtual void load_model (std::filesystem::path load_path);
