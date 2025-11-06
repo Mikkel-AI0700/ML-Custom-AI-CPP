c3e01d92ab70d2772b5d3c766f7b03f9f9a11d5e
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-11-06 16:35:00 +0800

    Added new minor C++ Scikit-Learn feature that constructs, loads, and saves datasets to avoid repeated logic

---
+++ header-source/header-source-files/cpp-utilities-source-files/loader.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <filesystem>
+#include <armadillo>
+#include "../../headers/cpp-utilities/loader.hpp"
+
+void DatasetOperations::construct_datasets () {
+    ;
+}
+
+void DatasetOperations::load_datasets () {
+    ;
+}
+
+void DatasetOperations::save_datasets (std::filesystem::path save_path) {
+    ;
+}
+
