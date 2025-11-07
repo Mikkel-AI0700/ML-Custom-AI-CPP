79c443781831549cd31b89d4a8fe435321f5dd4e
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-11-07 09:50:30 +0800

    Changed the void load_dataset () to void load_datasets ()

--- header-source/headers/cpp-utilities/loader.hpp
+++ header-source/headers/cpp-utilities/loader.hpp
@@ -1,10 +1,13 @@
+#include <vector>
 #include <filesystem>
 #include <armadillo>
 
 class DatasetOperations {
     public:
+        
+
         void construct_datasets ();
-        void load_dataset ();
+        void load_datasets ();
         void save_dataset (std::filesystem::path save_path);
 };
 
