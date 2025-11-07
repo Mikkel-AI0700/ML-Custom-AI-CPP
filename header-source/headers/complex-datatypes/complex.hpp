0159bb59e77b8b78609e37076f5ba958847b21e5
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-11-07 09:42:17 +0800

    Combined the logic inside linear_regression_complex.hpp to simplify the importing of complex datatypes

--- header-source/headers/complex-datatypes/complex.hpp
+++ header-source/headers/complex-datatypes/complex.hpp
@@ -1,10 +1,13 @@
 #ifndef COMPLEX_DATATYPES_HPP
 #define COMPLEX_DATATYPES_HPP
 
-#include <armadillo>
+#include <vector>
 #include <variant>
 #include <map>
+#include <armadillo>
 
 using HashMapParameters = std::map<std::string, std::variant<int, float, std::string, bool>>;
+using TrainTestData = std::vector<std::variant<arma::mat, arma::rowvec, arma::colvec>>;
+using DatasetsFilepaths = std::vector<std::filesystem::path>;
 
 #endif
