6a968fff6ce7c6ec9261e245cd2b6ae2b2ff47d6
Mikkel-AI0700 <mikeruai0700@gmail.com>
2025-11-07 19:20:15 +0800

    Added logic to have save paths

--- header-source/headers/cpp-utilities/loader.hpp
+++ header-source/headers/cpp-utilities/loader.hpp
@@ -1,13 +1,27 @@
-#include <vector>
 #include <filesystem>
 #include <armadillo>
+#include "../../headers/cpp-utilities/loader.hpp"
+
+struct SavePaths = {
+    std::filesystem::path regression_save_path = "/home/mikkel/Desktop/ai-projects/machine-learning/custom-ai-cpp/python-utilities/model-predictions/regressor";
+    std::filesystem::path classification_save_path = "/home/mikkel/Desktop/ai-projects/machine-learning/custom-ai-cpp/python-utilities/model-predictions/classification";
+    std::filesystem::path clustering_save_path = "/home/mikkel/Desktop/ai-projects/machine-learning/custom-ai-cpp/python-utilities/model-predictions/clustering";
+};
 
 class DatasetOperations {
     public:
-        
+        TrainTestData datasets_vector;
 
         void construct_datasets ();
-        void load_datasets ();
-        void save_dataset (std::filesystem::path save_path);
+        void load_datasets (
+            std::filesystem::path train_x,
+            std::filesystem::path train_y,
+            std::filesystem::path test_x,
+            std::filesystem::path test_y
+        );
+        void save_dataset (
+            std::string save_type
+            arma::colvec model_predictions,
+        );
 };
 
