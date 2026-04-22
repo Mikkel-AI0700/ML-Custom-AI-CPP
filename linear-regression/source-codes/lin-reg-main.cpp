edba15a4218d510161c2c98d9dbed98da14cc821
Mikkel-AI0700 <mikeruai0700@gmail.com>
2026-04-22 17:28:02 +0800

    Made some parameter changes and type definition changes

--- linear-regression/source-codes/lin-reg-main.cpp
+++ linear-regression/source-codes/lin-reg-main.cpp
@@ -5,14 +5,14 @@
 #include <armadillo>
 #include <filesystem>
 #include <variant>
-#include "base-headers/base.hpp"
-#include "base-headers/classifier_mixin.hpp"
-#include "cpp-utilities/loader.hpp"
-#include "complex-datatypes/complex_datatypes.hpp"
+#include "base.hpp"
+#include "classifier_mixin.hpp"
+#include "loader.hpp"
+#include "complex_datatypes.hpp"
 
 class LinearRegression: public BaseEstimator, public ClassifierMixin {
     public:
-        arma::colvec weights;
+        arma::vec weights;
         float bias;
         int epochs;
         float learning_rate;
@@ -20,20 +20,21 @@ class LinearRegression: public BaseEstimator, public ClassifierMixin {
         HashMapParameters parameter_constrains;
 
         LinearRegression(int epochs, float learning_rate, bool fit_intercept);
-        void fit (arma::mat& train_x, arma::colvec& train_y) override;
-        arma::colvec predict (arma::mat& test_x) override;
+        void fit (arma::mat& train_x, arma::vec& train_y) override;
+        arma::vec predict (arma::mat& test_x) override;
 
     private:
         void initialize_weights_bias (arma::mat& train_x);
-        arma::colvec compute_weights_gradients (arma::mat& train_x, arma::colvec& train_y, arma::colvec& predictions);
-        double compute_bias_gradients (arma::colvec& train_y, arma::colvec& predictions);
-        void update_weights (arma::colvec& computed_weight_gradients);
-        void update_bias (float copmputed_bias_gradient);
+        arma::vec compute_weights_gradients (arma::mat& train_x, arma::vec& train_y, arma::vec& predictions);
+        double compute_bias_gradients (arma::vec& train_y, arma::vec& predictions);
+        void update_weights (arma::vec& computed_weight_gradients);
+        void update_bias (float computed_bias_gradient);
 };
 
 LinearRegression::LinearRegression (int epochs, float learning_rate, bool fit_intercept) {
     this->epochs = epochs;
     this->learning_rate = learning_rate;
+    this->bias = 0.0;
     this->fit_intercept = fit_intercept;
     parameter_constrains = {
         {"epochs", epochs},
@@ -43,32 +44,36 @@ LinearRegression::LinearRegression (int epochs, float learning_rate, bool fit_in
 };
 
 void LinearRegression::initialize_weights_bias (arma::mat& train_x) {
-    weights = arma::zeros<arma::colvec>(train_x.n_cols);
+    weights = arma::zeros<arma::vec>(train_x.n_cols);
 
     if (fit_intercept) {
         bias = 0.0;
     }
 }
 
-arma::colvec LinearRegression::compute_weights_gradients (arma::mat& train_x, arma::colvec& train_y, arma::colvec& predictions) {
-    arma::colvec weights_gradient = 1.0 / train_x.n_rows * (train_x.t() * (predictions - train_y));
+arma::vec LinearRegression::compute_weights_gradients (
+    arma::mat& train_x,
+    arma::vec& train_y,
+    arma::vec& predictions
+) {
+    arma::vec weights_gradient = 1.0 / train_x.n_rows * (train_x.t() * (predictions - train_y));
     return weights_gradient;
 }
 
-double LinearRegression::compute_bias_gradients (arma::colvec& train_y, arma::colvec& predictions) {
+double LinearRegression::compute_bias_gradients (arma::vec& train_y, arma::vec& predictions) {
     double bias_gradient = 1.0 / train_y.n_rows * sum((predictions - train_y));
     return bias_gradient;
 }
 
-void LinearRegression::update_weights (arma::colvec& weights_gradient) {
-    weights = weights - learning_rate * weights_gradient;
+void LinearRegression::update_weights (arma::vec& computed_weight_gradients) {
+    weights = weights - learning_rate * computed_weight_gradients;
 }
 
-void LinearRegression::update_bias (float bias_gradient) {
-    bias = bias - learning_rate * bias_gradient;
+void LinearRegression::update_bias (float computed_bias_gradient) {
+    bias = bias - learning_rate * computed_bias_gradient;
 }
 
-void LinearRegression::fit (arma::mat& train_x, arma::colvec& train_y) {
+void LinearRegression::fit (arma::mat& train_x, arma::vec& train_y) {
     LinearRegression::initialize_weights_bias(train_x);
 
     for (int index = 0; index < epochs; index++) {
@@ -77,18 +82,18 @@ void LinearRegression::fit (arma::mat& train_x, arma::colvec& train_y) {
         std::cout << "[+] Bias: " << bias << std::endl;
 
         // Main predictions logic
-        arma::colvec predictions = (train_x * weights) + bias;
+        arma::vec predictions = (train_x * weights) + bias;
 
         // Weights and bias computing and updating
-        arma::colvec weight_gradient = LinearRegression::compute_weights_gradients(train_x, train_y, predictions);
-        double bias_gradient = LinearRegression::compute_bias_gradients(train_y, predictions);
-        LinearRegression::update_weights(weight_gradient);
-        LinearRegression::update_bias(bias_gradient);
+        arma::vec weight_gradient = compute_weights_gradients(train_x, train_y, predictions);
+        double bias_gradient = compute_bias_gradients(train_y, predictions);
+        update_weights(weight_gradient);
+        update_bias(bias_gradient);
     }
 }
 
-arma::colvec LinearRegression::predict (arma::mat& test_x) {
-    arma::colvec predictions = (test_x * weights) + bias;
+arma::vec LinearRegression::predict (arma::mat& test_x) {
+    arma::vec predictions = (test_x * weights) + bias;
     return predictions;
 }
 
@@ -115,16 +120,15 @@ int main (int argc, char* argv[]) {
 
     linreg_instance.fit(
         std::get<arma::mat>(dset_op.datasets_vector.at(0)),
-        std::get<arma::colvec>(dset_op.datasets_vector.at(1))
+        std::get<arma::vec>(dset_op.datasets_vector.at(1))
     );
 
-    arma::colvec predictions = linreg_instance.predict(
+    arma::vec predictions = linreg_instance.predict(
         std::get<arma::mat>(dset_op.datasets_vector.at(2))
     );
 
-    std::string regression_save_type = "regression";
     dset_op.save_dataset(
-        regression_save_type,
+        "regression",
         std::string(argv[7]),
         predictions
     ); 
