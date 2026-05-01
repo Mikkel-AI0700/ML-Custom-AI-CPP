a82716259507773efeea7b8844171fa69dfcc492
Mikkel-AI0700 <mikeruai0700@gmail.com>
2026-05-01 20:12:38 +0800

    Separated the logic from tree.cpp that counts the unique labels and returns their counts second into a source file

---
+++ header-source/header-source-files/cpp-utilities-source-files/linalg-operations.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <armadillo>
+#include "linalg-operations.hpp"
+
+UniqueFunctionReturns* unique (arma::vec& Y, bool return_counts) {
+    std::vector<int> discovered_labels;
+    std::vector<int> discovered_label_counts;
+    UniqueFunctionReturns* u_func_ret;
+
+    for (int index = 0; index < Y.n_elem; index++) {
+        if (index == 0) {
+            continue;
+        }
+
+        if (Y[index] != Y[index - 1]) {
+            discovered_labels.push_back(Y[index]);
+        }
+    }
+    u_func_ret->labels = discovered_labels;
+
+    if (return_counts) {
+        for (int outer_index = 0; outer_index < discovered_labels.size(); outer_index++) {
+            int label_counter = 0;
+            for (int inner_index = 0; inner_index < Y.n_elem; inner_index++) {
+                if (Y[outer_index] == discovered_labels[inner_index]) {
+                    label_counter++;
+                }
+            }
+            discovered_label_counts.push_back(label_counter);
+        }
+        u_func_ret->label_counts = discovered_label_counts;
+    }
+}
