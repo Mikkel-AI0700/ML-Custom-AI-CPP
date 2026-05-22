b893e307a6ebe716790e1f8a4cc574e1b293aea9
Mikkel-AI0700 <mikeruai0700@gmail.com>
2026-05-22 12:14:30 +0800

    Removed pointers and reverted to standard datatypes

--- header-source/headers/model-headers/decision-tree-header/tree-header.hpp
+++ header-source/headers/model-headers/decision-tree-header/tree-header.hpp
@@ -18,33 +18,34 @@ struct CreateNodeParameters {
 };
 
 struct RecursiveTreeBuilder {
-    arma::mat&                  left_x_mat;
-    arma::mat&                  right_x_mat;
-    arma::vec&                  left_y_vec;
-    arma::vec&                  right_y_vec;
+    arma::mat                   left_x_mat;
+    arma::mat                   right_x_mat;
+    arma::vec                   left_y_vec;
+    arma::vec                   right_y_vec;
     std::variant<int, float>    num_cond;
     float                       best_gain;
     int                         best_idx;
     int                         cat_cond;
 };
 
-struct SplitYieldParameters {
-    arma::mat&              x_feat_mat;
-    arma::vec&              y_target_vec;
-    std::vector<int>        num_cols;
-    std::vector<int>        cat_cols;
-};
-
 struct GeneratorVariables {
-    arma::mat&                  left_x_mat;
-    arma::mat&                  right_x_mat;
-    arma::vec&                  left_y_vec;
-    arma::vec&                  right_y_vec;
+    arma::mat                   left_x_mat;
+    arma::mat                   right_x_mat;
+    arma::vec                   left_y_vec;
+    arma::vec                   right_y_vec;
     std::string                 split_kind;
     std::variant<int, double>   num_thresh;
     int                         cat_thresh;
     int                         split_idx;
 };
+
+struct SplitYieldParameters {
+    arma::mat               x_feat_mat;
+    arma::vec               y_target_vec;
+    std::vector<int>        num_cols;
+    std::vector<int>        cat_cols;
+};
+
 
 class LeafNode {
     public:
