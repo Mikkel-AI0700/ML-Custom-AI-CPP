16098e8622c30636e51b924ed8867658a03df544
Mikkel-AI0700 <mikeruai0700@gmail.com>
2026-06-03 09:35:02 +0800

    Added std::optional to num_cond and cat_cond and used RecursiveTreeBuilder struct

--- header-source/headers/model-headers/decision-tree-header/tree-header.hpp
+++ header-source/headers/model-headers/decision-tree-header/tree-header.hpp
@@ -24,10 +24,10 @@ struct RecursiveTreeBuilder {
     arma::mat                                  right_x_mat;
     arma::vec                                  left_y_vec;
     arma::vec                                  right_y_vec;
-    float                                      num_cond;
+    std::optional<float>                       num_cond;
     float                                      best_gain;
     int                                        best_idx;
-    int                                        cat_cond;
+    std::optional<int>                         cat_cond;
 };
 
 struct SplitYieldParameters {
@@ -133,7 +133,9 @@ class DecisionTreeClassifier: public BaseEstimator, public ClassifierMixin {
             GeneratorVariables& generator_parameters
         );
         std::unique_ptr<Node> create_node (
-            CreateNodeParameters& node_parameters
+            RecursiveTreeBuilder& rec_tree_build,
+            bool create_decision_node,
+            bool create_leaf_node
         );
         std::unique_ptr<Node> build_decision_tree (
             arma::mat& X, 
