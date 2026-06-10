8d36193188c0573b72b8a734755795302f259289
Mikkel-AI0700 <mikeruai0700@gmail.com>
2026-06-10 10:29:13 +0800

    Added CMAKE_EXPORT_COMPILE_COMMANDS=ON

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -57,5 +57,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/CPP_SCIKIT_LEARN.dir/DependInfo.cmake"
+  "CMakeFiles/main.dir/DependInfo.cmake"
   )
