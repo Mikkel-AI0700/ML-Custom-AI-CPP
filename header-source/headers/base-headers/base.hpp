4f3789dceb9f1474e63e3edbc8c270978ee6a919
Mikkel-AI0700 <mikeruai0700@gmail.com>
2026-08-06 10:24:40 +0800

    refactor(base): return arma::vec from predict_proba/predict_proba_log

--- header-source/headers/base-headers/base.hpp
+++ header-source/headers/base-headers/base.hpp
@@ -13,8 +13,8 @@ class BaseEstimator {
 
         virtual void fit (arma::mat& train_x, arma::vec& train_y);
         virtual arma::vec predict (arma::mat& test_x);
-        virtual arma::rowvec predict_proba (arma::mat& test_x);
-        virtual arma::rowvec predict_proba_log (arma::mat& test_x);
+        virtual arma::vec predict_proba (arma::mat& test_x);
+        virtual arma::vec predict_proba_log (arma::mat& test_x);
 
         virtual void save_model (std::string model_save_name, std::filesystem::path save_path);
         virtual void load_model (std::filesystem::path load_path);
