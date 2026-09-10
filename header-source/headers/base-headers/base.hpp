d57087d081bb62ef3dd2f03da0385493574448ce
Mikkel-AI0700 <mikeruai0700@gmail.com>
2026-09-11 06:31:18 +0800

    base.hpp: Changed the function parameter signatures to handle single and batch inference

--- header-source/headers/base-headers/base.hpp
+++ header-source/headers/base-headers/base.hpp
@@ -12,9 +12,9 @@ class BaseEstimator {
         virtual void set_parameters (HashMapParameters original_hyperparameters, HashMapParameters user_hyperparameters);
 
         virtual void fit (arma::mat& train_x, arma::vec& train_y);
-        virtual arma::vec predict (arma::mat& test_x);
-        virtual arma::vec predict_proba (arma::mat& test_x);
-        virtual arma::vec predict_proba_log (arma::mat& test_x);
+        virtual std::variant<int, arma::vec> predict (std::variant<arma::vec, arma::mat>& test_x);
+        virtual std::variant<arma::vec, arma::mat> predict_proba (std::variant<arma::vec, arma::mat>& test_x);
+        virtual std::variant<arma::vec, arma::mat> predict_proba_log (std::variant<arma::vec, arma::mat>& test_x);
 
         virtual void save_model (std::string model_save_name, std::filesystem::path save_path);
         virtual void load_model (std::filesystem::path load_path);
